b63420319ecf7c0b5fe636dd100261a82222aacb
Max Yezerskyi <m.yezerskyi@gmail.com>
2018-07-30 19:05:41 +0300

    old project

---
+++ students/views/groups.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.forms import ModelForm
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
+
+from ..models import Group
+from ..util import paginate
+
+
+def groups_list(request):
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title',):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # apply pagination, 2 groups per page
+    context = paginate(groups, 2, request, {}, var_name='groups')
+
+    return render(request, 'students/groups_list.html', context)
+
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+
+    def __init__(self, *args, **kwargs):
+        super(GroupForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+
+        # add form or edit form
+        if kwargs['instance'] is None:
+            add_form = True
+        else:
+            add_form = False
+
+        # set form tag attributes
+        if add_form:
+            self.helper.form_action = reverse('groups_add')
+        else:
+            self.helper.form_action = reverse('groups_edit',
+                kwargs={'pk': kwargs['instance'].id})
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # add buttons
+        if add_form:
+            submit = Submit('add_button', u'Додати',
+                css_class="btn btn-primary")
+        else:
+            submit = Submit('save_button', u'Зберегти',
+                css_class="btn btn-primary")
+        self.helper.layout[-1] = FormActions(
+            submit,
+            Submit('cancel_button', u'Скасувати', css_class="btn btn-link"),
+        )
+
+class BaseGroupFormView(object):
+
+    def get_success_url(self):
+        return u'%s?status_message=Зміни успішно збережено!' \
+            % reverse('groups')
+
+    def post(self, request, *args, **kwargs):
+        # handle cancel button
+        if request.POST.get('cancel_button'):
+            return HttpResponseRedirect(reverse('groups') +
+                u'?status_message=Зміни скасовано.')
+        else:
+            return super(BaseGroupFormView, self).post(
+                request, *args, **kwargs)
+
+class GroupAddView(BaseGroupFormView, CreateView):
+    model = Group
+    form_class = GroupForm
+    template_name = 'students/groups_form.html'
+
+class GroupUpdateView(BaseGroupFormView, UpdateView):
+    model = Group
+    form_class = GroupForm
+    template_name = 'students/groups_form.html'
+
+class GroupDeleteView(BaseGroupFormView, DeleteView):
+    model = Group
+    template_name = 'students/groups_confirm_delete.html'
---
+++ studentsdb/settings.py
@@ -0,0 +1,104 @@
+"""
+Django settings for studentsdb project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+from django.conf import global_settings
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+PORTAL_URL = 'http://localhost:8000'
+
+# email settings
+# please, set here you smtp server details and your admin email
+ADMIN_EMAIL = 'admin@studentsdb.com'
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'vitaliypodoba@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$lrm#z+qs)(&2rkr)tnui0m0%7r%)s!)9ni$#ug_vjmce+5x%!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crispy_forms',
+    'students',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'studentsdb.urls'
+
+WSGI_APPLICATION = 'studentsdb.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+# We moved DATABASES variable to db.py module which added to .gitignore
+# so we don't keep mysql passwords in repository
+from .db import DATABASES
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'uk'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
+TEMPLATE_CONTEXT_PROCESSORS = \
+    global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    "django.core.context_processors.request",
+    "students.context_processors.groups_processor",
+    "studentsdb.context_processors.students_proc",
+)
